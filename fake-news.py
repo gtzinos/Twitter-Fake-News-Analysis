108e96e741bbf63570ed656c40e775808912c9c2
atheodosiou <anastasios.theodosiou@gmail.com>
2018-04-12 21:14:34 +0300

    Adding some code for statistics

--- fake-news.py
+++ fake-news.py
@@ -15,11 +15,11 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
-selected_user='WSJ'
+selected_user = 'WSJ'
 
 print('Getting data for user: '+selected_user)
 
-item=auth_api.get_user(selected_user)
+item = auth_api.get_user(selected_user)
 
 print("name: " + item.name)
 print("screen_name: " + item.screen_name)
@@ -29,7 +29,20 @@ print("friends_count: " + str(item.friends_count))
 print("followers_count: " + str(item.followers_count))
 
 
-stuff = auth_api.user_timeline(screen_name = item.screen_name, count = 100, include_rts = True)
-print('More info:')
-print(stuff)
+#stuff = auth_api.user_timeline(screen_name = item.screen_name, count = 100, include_rts = True)
+#print('More info:')
+# print(stuff)
 
+#Calculate the average number of tweets per day
+
+tweets = item.statuses_count
+account_created_date = item.created_at
+delta = datetime.utcnow() - account_created_date
+account_age_days = delta.days
+print("Account age (in days): " + str(account_age_days))
+
+print("Account age (in years): " + str(account_age_days/365))
+
+if account_age_days > 0:
+    print("Average tweets per day: " + "%.2f" %
+          (float(tweets)/float(account_age_days)))
