532c0f03f42eeb12425e1d987cb9bb3bce7bea62
atheodosiou <anastasios.theodosiou@gmail.com>
2018-04-12 21:26:08 +0300

    A sample of using Tweepy's Cursor

--- fake-news.py
+++ fake-news.py
@@ -33,7 +33,7 @@ print("followers_count: " + str(item.followers_count))
 #print('More info:')
 # print(stuff)
 
-#Calculate the average number of tweets per day
+# Calculate the average number of tweets per day
 
 tweets = item.statuses_count
 account_created_date = item.created_at
@@ -46,3 +46,33 @@ print("Account age (in years): " + str(account_age_days/365))
 if account_age_days > 0:
     print("Average tweets per day: " + "%.2f" %
           (float(tweets)/float(account_age_days)))
+
+# Url from a single tweet by WSJ
+# https://twitter.com/i/web/status/984269901333450753 <-- TWEET ID
+
+
+# Not working
+hashtags = []
+mentions = []
+tweet_count = 0
+end_date = datetime.utcnow() - timedelta(days=30)
+for status in Cursor(auth_api.user_timeline, id=selected_user).items():
+    tweet_count += 1
+    if hasattr(status, "entities"):
+        entities = status.entities
+        if "hashtags" in entities:
+            for ent in entities["hashtags"]:
+                if ent is not None:
+                    if "text" in ent:
+                        hashtag = ent["text"]
+                        if hashtag is not None:
+                            hashtags.append(hashtag)
+        if "user_mentions" in entities:
+            for ent in entities["user_mentions"]:
+                if ent is not None:
+                    if "screen_name" in ent:
+                        name = ent["screen_name"]
+                        if name is not None:
+                            mentions.append(name)
+    if status.created_at < end_date:
+        break
