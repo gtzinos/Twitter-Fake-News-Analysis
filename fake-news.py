e5c109e2bed6870544bc5b58604524e07e1ffa59
atheodosiou <anastasios.theodosiou@gmail.com>
2018-04-12 20:48:00 +0300

    Initial commit

---
+++ fake-news.py
@@ -0,0 +1,35 @@
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
+
+consumer_key = "PmOtnHDeLol5iTFJfT0SzhngX"
+consumer_secret = "VPVLEZKDNg0pQn3FAoXepwUuR8cRM0vQ5B2xKJnZAvvDel67eD"
+access_token = "296863841-ySiLADayokktc0JFtVGcQbd7afwbbVR9B4HxcfTi"
+access_token_secret = "QU6QdwWhe1emH4qaWyaxw9gsR9iiUFc3jWa9cOCU5Jztu"
+
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+auth_api = API(auth)
+
+selected_user='WSJ'
+
+print('Getting data for user: '+selected_user)
+
+item=auth_api.get_user(selected_user)
+
+print("name: " + item.name)
+print("screen_name: " + item.screen_name)
+print("description: " + item.description)
+print("statuses_count: " + str(item.statuses_count))
+print("friends_count: " + str(item.friends_count))
+print("followers_count: " + str(item.followers_count))
+
+
+stuff = auth_api.user_timeline(screen_name = item.screen_name, count = 100, include_rts = True)
+print('More info:')
+print(stuff)
+
