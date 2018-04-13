d8cb5b0db45edde76072d97ce104539a28901e85
George Tzinos <geotzinos@gmail.com>
2018-04-13 13:24:42 +0300

    Seperate config file

--- fake-news.py
+++ fake-news.py
@@ -4,12 +4,7 @@ from tweepy import Cursor
 from datetime import datetime, date, time, timedelta
 from collections import Counter
 import sys
-
-consumer_key = "PmOtnHDeLol5iTFJfT0SzhngX"
-consumer_secret = "VPVLEZKDNg0pQn3FAoXepwUuR8cRM0vQ5B2xKJnZAvvDel67eD"
-access_token = "296863841-ySiLADayokktc0JFtVGcQbd7afwbbVR9B4HxcfTi"
-access_token_secret = "QU6QdwWhe1emH4qaWyaxw9gsR9iiUFc3jWa9cOCU5Jztu"
-
+from config import *
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
