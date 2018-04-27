40266d208b14e484f827ce4e849c17915cb2bfcf
Marinos Zagkotsis <zagkotsis@csd.auth.gr>
2018-04-27 23:53:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-
-from tweepy import Cursor
+from tweepy import *
 from datetime import datetime, date, time, timedelta
 from collections import Counter
 import sys
 from config import *
 from user import *
+from tweepy import parsers
 
 auth_api = login(consumer_key, consumer_secret, access_token, access_token_secret)
 
 loggedUser = getUser(user_tweets, auth_api)
-
-#stuff = auth_api.user_timeline(screen_name = loggedUser.screen_name, count = 100, include_rts = True)
-#print('More info:')
-# print(stuff)
 
 # Calculate the average number of tweets per day
 printUserDetails(loggedUser)
@@ -20,6 +16,7 @@ printUserDetails(loggedUser)
 
 # Url from a single tweet by WSJ
 # https://twitter.com/i/web/status/984269901333450753 <-- TWEET ID
+
 
 #get most mentioned hashtags and users
 # getHashtagsUsers(auth_api,user_tweets)
@@ -32,7 +29,12 @@ print("----------------------------")
 # print('More info:')
 # print(stuff)
 
-getTweets(loggedUser, auth_api)
+tweetIdList = getTweets(loggedUser, auth_api)
+print("-------------------------")
+pprint.pprint(tweetIdList[0])
+print("-------------------------")
+retweets = getRetweeters(tweetIdList[0],auth_api)
+print("------------RETWEET INFO-------------")
+print(retweets)
 # getTweetsByQuery(auth_api, "Tsipras", "en")
 # getTweets2(auth_api)
-
