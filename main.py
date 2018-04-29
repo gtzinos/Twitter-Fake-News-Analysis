ba31360685f7ff53490e20ae8e3cc3723dcdd7e1
Marinos Zagkotsis <zagkotsis@csd.auth.gr>
2018-04-29 22:01:25 +0300

    Update main.py
    Add next account

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from tweepy import parsers
 
 auth_api = login(consumer_key, consumer_secret, access_token, access_token_secret)
 
-loggedUser = getUser(user_tweets, auth_api)
-
+WSJUser = getUser(WSJ_tweets, auth_api)
+HuzlersUser = getUser(Huzlers_tweets,auth_api)
 # Calculate the average number of tweets per day
-printUserDetails(loggedUser)
-
+printUserDetails(WSJUser)
+print("------------------------------")
+printUserDetails(HuzlersUser)
 
 # Url from a single tweet by WSJ
 # https://twitter.com/i/web/status/984269901333450753 <-- TWEET ID
@@ -29,12 +30,21 @@ print("----------------------------")
 # print('More info:')
 # print(stuff)
 
-tweetIdList = getTweets(loggedUser, auth_api)
+tweetIdListWSJ = getTweets(WSJUser, auth_api)
 print("-------------------------")
-pprint.pprint(tweetIdList[0])
+pprint.pprint(tweetIdListWSJ[0])
 print("-------------------------")
-retweets = getRetweeters(tweetIdList[0],auth_api)
-print("------------RETWEET INFO-------------")
-print(retweets)
+retweetsWSJ = getRetweeters(tweetIdListWSJ[0],auth_api)
+print("------------RETWEET INFO WSJ-------------")
+print(retweetsWSJ)
 # getTweetsByQuery(auth_api, "Tsipras", "en")
 # getTweets2(auth_api)
+print("--------------------------------------")
+
+tweetIdListHuzler = getTweets(HuzlersUser, auth_api)
+print("-------------------------")
+pprint.pprint(tweetIdListHuzler[0])
+print("-------------------------")
+retweetsHuzler = getRetweeters(tweetIdListHuzler[0],auth_api)
+print("------------RETWEET INFO TheHuzlers-------------")
+print(retweetsHuzler)
