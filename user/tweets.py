3ec5d16921758bf67db21f69b17d10cfb081b8b2
Marinos Zagkotsis <zagkotsis@csd.auth.gr>
2018-05-05 17:06:15 +0300

    Update

--- user/tweets.py
+++ user/tweets.py
@@ -7,8 +7,8 @@ from collections import OrderedDict
 
 
 FILE_WSJ = 'WSJ.json'
-
-def getTweets (loggedUser,auth_api):
+retweetCounter = 0
+""" def getTweets (loggedUser,auth_api):
     counter = 0
     tweetDict = {}
     with open(FILE_WSJ,'w') as f_out:
@@ -25,9 +25,21 @@ def getTweets (loggedUser,auth_api):
     print(tweetRetweetNoList[-3:])
     tweetIdList = list(sortedTweetDict.keys())
     print(tweetIdList[-3:])
-    return tweetIdList[-3:]
-    # for k, v in sortedTweetDict.items():
-    #     print k, v
+    return tweetIdList[-3:] """
+###TODO FIX 
+def getTweets(loggedUser,auth_api):
+    tweetList = list()
+    p=1
+    for tweet in auth_api.user_timeline(screen_name = loggedUser.screen_name):
+        print("TWEET:", tweet.text)
+        for reTweet in auth_api.retweets(tweet.id, count=100):
+            print("USER:", reTweet.user.screen_name)
+            tweetList.append(reTweet.user.screen_name)
+            p = p +1
+    print(p)
+    return tweetList
+    
+
 
 def getTweetsByQuery(auth_api, query,languageCode):
     for tweet in tweepy.Cursor(auth_api.search, q=query, lang=languageCode).items():
@@ -43,17 +55,23 @@ def getTweets2(auth_api):
         print tweet.text
 
 def getRetweets(tweetId,auth_api):
-    # results = auth_api.retweets(id=tweetId, count=100)
-    # return results
     p=1
     while True:
-        results = auth_api.retweets(id=tweetId, page= p, count=100)
+        results = auth_api.retweets(id=tweetId, page= p)
         if results:
-            print(results['author'])
+            print(results)  
             p=p+1
         else:
             break
+    print (p)
 
 def getRetweeters(tweetId,auth_api):
     results = auth_api.retweeters(id=tweetId)
     return results
+
+def PrintMembers(obj):
+    for attribute in dir(obj):
+        
+        #We don't want to show built in methods of the class
+        if not attribute.startswith('__'):
+            print(attribute)
