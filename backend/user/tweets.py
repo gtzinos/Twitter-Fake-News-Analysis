d2c56c789a099ab54258894661a129a9cd6954bd
Marinos Zagkotsis <zagkotsis@csd.auth.gr>
2018-05-14 10:46:59 +0300

    Write to JSON
    remove unnecessary code

--- backend/user/tweets.py
+++ backend/user/tweets.py
@@ -5,103 +5,60 @@ import json
 import pprint
 from collections import OrderedDict
 
-#This is for testing only
-"""def getTweets (loggedUser,auth_api):
-    
-    retweeterCounter = 1
-    counter = 0
-    p=0
-    tweetDict = {}
-    tweetList = list()
-    for tweet in tweepy.Cursor(auth_api.user_timeline, screen_name = loggedUser.screen_name, include_rts = True).items():
-        tweetDict.update({tweet.id:tweet.retweet_count})
-        counter += 1
-        
-    print("Finished... with count: ",counter)
-    sortedTweetDict = OrderedDict(sorted(tweetDict.items(), key=lambda x:x[1]))
-    tweetRetweetNoList = list(sortedTweetDict.values())
-    print("How many retweets with top 3: " + str(tweetRetweetNoList[-3:]))
-    tweetIdList = list(sortedTweetDict.keys())
-    print("The top 3 tweets with ID: " + str(tweetIdList[-3:]))
-    topThree = tweetIdList[-3:]
-    for tweetId in topThree:
-        print("---------------------------------------")
-        print("Printing retweeters for tweet: " + str(retweeterCounter) +":" +str(tweetId))
-        for retweet in auth_api.retweets(tweetId,page=p):
-            try:
-                print("USER:", retweet.user.screen_name)
-                tweetList.append(retweet.user.screen_name)
-                p = p +1
-            except tweepy.TweepError:
-                print("waiting...")
-                continue
-        retweeterCounter += 1
-    
-    print("Retweeters found: "+str(p))
-    return tweetList """
-#This is production
+
 def getTweets (loggedUser,auth_api):
-    
+    dataJSON = {}
+    i = 0
     retweeterCounter = 0
     counter = 0
     p=0
     tweetDict = {}
-    tweetList = list()
+
     for tweet in tweepy.Cursor(auth_api.user_timeline, screen_name = loggedUser.screen_name, include_rts = True).items():
         tweetDict.update({tweet.id:tweet.retweet_count})
         counter += 1
         
     print("Finished... with count: ",counter)
     sortedTweetDict = OrderedDict(sorted(tweetDict.items(), key=lambda x:x[1]))
-    tweetRetweetNoList = list(sortedTweetDict.values())
-    print("How many retweets with top 3: " + str(tweetRetweetNoList[-3:]))
-    tweetIdList = list(sortedTweetDict.keys())
-    print("The top 3 tweets with ID: " + str(tweetIdList[-3:]))
-    topThree = tweetIdList[-3:]
+    temp = list(sortedTweetDict.values())
+    tweetRetweetNoList = temp[-3:]
+    print("Retweet No of the top 3: " + str(temp[-3:]))
+    temp = list(sortedTweetDict.keys())
+    tweetIdList = temp[-3:]
+    print("The top 3 tweets with ID: " + str(temp[-3:]))
+    topThree = temp[-3:]
     print("---------------------------------------")
-    print("Printing retweeters for tweet: " +str(topThree[2]))
-    while(True):
-        retweets = auth_api.retweets(topThree[2],page=p)
-        for retweet in retweets:
-            if(retweet):
-                try:
-                    print("USER:", retweet.user.screen_name)
-                    tweetList.append(retweet.user.screen_name)
-                    retweeterCounter += 1
-                except tweepy.TweepError:
-                    print("waiting...")
-                    continue
-        p = p +1
-    print("Retweeters found: "+str(retweeterCounter) + "in #" +str(p) +"pages")
-    return tweetList
-
-def getTweetsByQuery(auth_api, query,languageCode):
-    for tweet in tweepy.Cursor(auth_api.search, q=query, lang=languageCode).items():
-        print tweet
-
-def getTweets2(auth_api):
-    for tweet in tweepy.Cursor(auth_api.search,
-                           q="WSJ",
-                           rpp=100,
-                           result_type="recent",
-                           include_entities=True,
-                           lang="en").items():
-        print tweet.text
-
-"""def getRetweets(tweetId,auth_api):
-    p=1
-    while True:
-        results = auth_api.retweets(id=tweetId, page= p)
-        if results:
-            print(results)  
-            p=p+1
+    while(i < 3):  
+        print("Tweet no: "+ str(i) + "->" + "Printing retweeters for tweet: " +str(topThree[i]))
+        if(auth_api.retweets(topThree[i])!= None): 
+            
+            retweets = auth_api.retweets(topThree[i],page=p)
+            for retweet in retweets:
+                if(retweet):
+                    try:
+                        #print("USER:", retweet.user.screen_name)
+                        dataJSON.update({'tweetObject' : [{'originalTweet':{'originalTweetId':tweetIdList[i],'retweetCount':tweetRetweetNoList[i]},'retweet':{'retweetCreatedAt':str(retweet.created_at),
+                        'retweetId':retweet.id,'retweeterName':retweet.user.screen_name}}]})
+                        #tweetList.append(retweet.user.screen_name)
+                        retweeterCounter += 1
+                        if(loggedUser.screen_name == 'WSJ'):
+                            with open('WSJ.json','a') as file:
+                                file.write(json.dumps(dataJSON,file,sort_keys=True,indent=4, separators=(',', ': ')))
+                        elif(loggedUser.screen_name == 'TheHuzlers'):
+                            with open('Huzzlers.json','a') as file:
+                                file.write(json.dumps(dataJSON,file,sort_keys=True,indent=4, separators=(',', ': ')))
+                        else:
+                            print("File not found...")
+                            break
+                    except tweepy.TweepError:
+                        print("waiting...")
+                        continue
+            p = p + 1
         else:
-            break
-    print (p)"""
+            i = i + 1
+    print("Retweeters found: "+str(retweeterCounter) + " in #" +str(p) +"pages")
 
-"""def getRetweeters(tweetId,auth_api):
-    results = auth_api.retweeters(id=tweetId)
-    return results"""
+
 
 def PrintMembers(obj):
     for attribute in dir(obj):
