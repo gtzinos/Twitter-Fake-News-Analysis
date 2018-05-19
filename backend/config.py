ea0b0f15ab4b8b3e2f80706be8fdc281862922a2
Marinos Zagkotsis <zagkotsis@csd.auth.gr>
2018-05-19 20:35:36 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,3 @@ consumer_key = "PmOtnHDeLol5iTFJfT0SzhngX"
 consumer_secret = "VPVLEZKDNg0pQn3FAoXepwUuR8cRM0vQ5B2xKJnZAvvDel67eD"
 access_token = "296863841-ySiLADayokktc0JFtVGcQbd7afwbbVR9B4HxcfTi"
 access_token_secret = "QU6QdwWhe1emH4qaWyaxw9gsR9iiUFc3jWa9cOCU5Jztu"
-WSJ_tweets = 'WSJ'
-Huzlers_tweets = 'TheHuzlers'
