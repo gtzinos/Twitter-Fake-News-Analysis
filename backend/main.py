a964e00d78f18b811b1482986947aad766e07ce7
George Tzinos <geotzinos@gmail.com>
2018-05-27 02:24:51 +0300

    Rename main

--- backend/main.py
+++ backend/main.py
@@ -1,63 +1,35 @@
-#from tweepy import *
+from config.database import *
+from config.credentials import *
+from database.connect import openConnection
+from database.users import Users
+from twitter.login import *
+from user.info import *
+from twitter.owner import *
 
-from user.friendship import readDataFromJSON
+# Open connection
+db = openConnection(db_hostname, db_name, db_port, db_username, db_password, db_authMechanism)
 
-#auth_api = login(consumer_key, consumer_secret, access_token, access_token_secret)
-
-# WSJ_tweets = 'WSJ'
-Huzlers_tweets = 'TheHuzlers'
-
-# WSJUser = getUser(WSJ_tweets, auth_api)
-#HuzlersUser = getUser(Huzlers_tweets,auth_api)
+# Login user
+auth_api = login(consumer_key, consumer_secret, access_token, access_token_secret)
+# Owner login
+loggedInUser = getUser(username, auth_api)
 # Calculate the average number of tweets per day
-# printUserDetails(WSJUser)
-#print("------------------------------")
-# printUserDetails(HuzlersUser)
+printUserDetails(loggedInUser)
 
-# Url from a single tweet by WSJ
-# https://twitter.com/i/web/status/984269901333450753 <-- TWEET ID
+# Insert user
+userModel = Users(users_table_name)
+userModel.insert_if_not_exists(db, loggedInUser.id, loggedInUser._json)
 
+# Get all tweets from database
+allUserTweets = Tweets(tweets_table_name).find_all_by_owner_id(db, loggedInUser.id)
 
-#get most mentioned hashtags and users
-# getHashtagsUsers(auth_api,user_tweets)
-#print("----------------------------")
-# getStatus(auth_api)
+# Get Tweets from owner
+# getTweets(loggedInUser, auth_api, db)
 
-#Get Tweets
+# Get Retweets for top 3 Tweets
+# getRetweetsOfTop3(loggedInUser, auth_api, db)
 
-# stuff = auth_api.user_timeline(screen_name = loggedUser.screen_name, count = 100, include_rts = True)
-# print('More info:')
-# print(stuff)
+# Get follower of retweets of 3 top tweets
+getFollowers(loggedInUser, auth_api, db)
 
-# print("------------RETWEET INFO WSJ-------------")
-# getTweetsWSJ(WSJUser, auth_api)
-#print("------------RETWEET INFO TheHuzlers-------------")
-#getTweetsHuzlers(HuzlersUser,auth_api)
-
-#print("------------THE END-------------")
-#tweetIdListWSJ = getTweets(WSJUser, auth_api)
-#print("-------------------------")
-#pprint.pprint(tweetIdListWSJ[0])
-#print("-------------------------")
-
-
-#print("--------------------------------------")
-
-# tweetIdListHuzler = getTweets(HuzlersUser, auth_api)
-# print("-------------------------")
-# pprint.pprint(tweetIdListHuzler[0])
-# print("-------------------------")
-# retweetsHuzler = getRetweets(tweetIdListHuzler[1],auth_api)
-# retweetsHuzler = getRetweeters(tweetIdListHuzler[1],auth_api)
-
-# 
-# print(retweetsHuzler)
-
-
-#Run it only one time!
-#insertData()
-
-#showData()
-
-#checkFriendship(2302467404,'BrianZ_CR')
-readDataFromJSON('h_1.json')
+# Get followers for Retweets of top 3 Tweets
