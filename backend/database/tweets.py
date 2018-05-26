844354ddbe023046c706db3be6f3cc8572c7c684
George Tzinos <geotzinos@gmail.com>
2018-05-26 19:11:40 +0300

    Database tables + move old to refactor

---
+++ backend/database/tweets.py
@@ -0,0 +1,10 @@
+from .databaseTable import DatabaseTable
+
+class Tweets(DatabaseTable):
+
+    def __init__(self, name):
+        DatabaseTable.__init__(self, name)
+
+    def find_all_by_owner_id(self, db, owner_id):
+        return db[self.name].find({'owner_id': owner_id})
+
