3e196d3323a6a01d64cdcfec08528f8a302371c5
George Tzinos <geotzinos@gmail.com>
2018-05-26 19:10:52 +0300

    Refactoring

---
+++ backend/config/database.py
@@ -0,0 +1,8 @@
+db_hostname = "localhost"
+db_name = "twitter"
+db_port = 27017
+
+users_table_name = "users"
+tweets_table_name = "tweets"
+retweets_table_name = "retweets"
+followers_table_name = "followers"
---
+++ backend/twitter/login.py
@@ -0,0 +1,30 @@
+from tweepy import OAuthHandler
+from tweepy import API
+
+
+def getUser(user_tweets, auth_api):
+    print('Getting data for user: ' + user_tweets)
+
+    logged_user = auth_api.get_user(user_tweets)
+
+    print("name: " + logged_user.name)
+    print("screen_name: " + logged_user.screen_name)
+    print("description: " + logged_user.description)
+    print("statuses_count: " + str(logged_user.statuses_count))
+    print("-------------------------------------")
+    print("friends_count: " + str(logged_user.friends_count))
+    print("-------------------------------------")
+    print("followers_count: " + str(logged_user.followers_count))
+    print("-------------------------------------")
+
+    return logged_user
+
+
+def login(consumer_key, consumer_secret, access_token, access_token_secret):
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return API(auth, retry_count=5,
+               retry_delay=10,
+               retry_errors=set([401, 404, 500, 503]),
+               wait_on_rate_limit=True,
+               wait_on_rate_limit_notify=True)
