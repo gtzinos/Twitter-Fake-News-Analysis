5f002d35459115e4827edf24e57e4e2b0ba3ff3a
George Tzinos <geotzinos@gmail.com>
2018-05-27 20:50:43 +0300

    Server debug

--- backend/twitter/owner.py
+++ backend/twitter/owner.py
@@ -77,8 +77,9 @@ def are_followers(auth_api, sourceUserId, targetUserId):
     return final_decision
 
 
-def getFollowers(loggedInUser, auth_api):
+def getFollowers(loggedInUserTweepy, auth_api):
     db = openConnection(db_hostname, db_name, db_port, db_username, db_password, db_authMechanism)
+    loggedInUser = loggedInUserTweepy._json
 
     retweetsModel = Retweets(retweets_table_name)
     followersModel = Followers(followers_table_name)
@@ -87,48 +88,45 @@ def getFollowers(loggedInUser, auth_api):
     # Check all retweeters with owner page
 
     # get all not in followers collection
-    available_retweeters = retweetsModel.find_all_not_in_followers(db, followers_table_name, loggedInUser.id)
+    available_retweeters = retweetsModel.find_all_not_in_followers(db, followers_table_name, loggedInUser['id'])
 
     available_retweeters_memory = get_retweeters_memory(available_retweeters)
 
-    db.close()
+    print("Step 1 Followers")
 
     for available_retweeter_memory in available_retweeters_memory:
-        are_followers_result = are_followers(auth_api, available_retweeter_memory['user']['id'], loggedInUser.id)
+        are_followers_result = are_followers(auth_api, available_retweeter_memory['user']['id'], loggedInUser['id'])
 
         if are_followers_result:
-            db = openConnection(db_hostname, db_name, db_port, db_username, db_password, db_authMechanism)
 
             followersModel.insert_if_not_exists(db, {
                 "source": available_retweeter_memory,
-                "target": {'user': loggedInUser._json},
-                "from_owner": loggedInUser._json,
+                "target": {'user': loggedInUser},
+                "from_owner": loggedInUser,
                 "hop_number": hop_number
             })
-
-            db.close()
 
     # Check all available with last insert
     found_followers = 1
 
+    print("Step 2 Followers")
+
     try:
         while found_followers > 0:
+            print(hop_number)
             hop_number += 1
+
+            print("hop: " + str(hop_number))
+
             # clear value
             found_followers = 0
             # get all not in followers collection
 
-        
-
-            db = openConnection(db_hostname, db_name, db_port, db_username, db_password, db_authMechanism)
-
-            available_retweeters = retweetsModel.find_all_not_in_followers(db, followers_table_name, loggedInUser.id)
+            available_retweeters = retweetsModel.find_all_not_in_followers(db, followers_table_name, loggedInUser['id'])
             available_retweeters_memory = get_retweeters_memory(available_retweeters)
 
-            last_inserted_followers = followersModel.get_last_inserted_hop(db, loggedInUser.id)
+            last_inserted_followers = followersModel.get_last_inserted_hop(db, loggedInUser['id'])
             last_inserted_followers_memory = get_last_inserted_memory(last_inserted_followers)
-
-            db.close()
 
             for available_retweeter_memory in available_retweeters_memory:
                 for last_inserted_follower_memory in last_inserted_followers_memory:
@@ -139,17 +137,13 @@ def getFollowers(loggedInUser, auth_api):
                     are_followers_result = are_followers(auth_api, available_retweeter_memory['user']['id'],
                                                         last_inserted_follower_memory[who_to_compare]['user']['id'])
                     if are_followers_result:
-                        db = openConnection(db_hostname, db_name, db_port, db_username, db_password, db_authMechanism)
-
                         followersModel.insert_if_not_exists(db, {
                             "source": available_retweeter_memory,
                             "target": last_inserted_follower_memory[who_to_compare],
-                            "from_owner": loggedInUser._json,
+                            "from_owner": loggedInUser,
                             "hop_number": hop_number
                         })
                         found_followers += 1
-
-                        db.close()
     except Exception as e:
         print("ERROR!")
         print(e)
