b0a59a6d941078110476bc07df5f65fe558261da
George Tzinos <geotzinos@gmail.com>
2018-05-28 02:41:07 +0300

    FIX About page

--- twitter-front/src/app/components/about/about.component.ts
+++ twitter-front/src/app/components/about/about.component.ts
@@ -12,15 +12,7 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    let randomDevs = [{
-      name: "Ilias Karatsin",
-      title: "Full Stack Developer",
-      photo: "https://scontent.fskg1-1.fna.fbcdn.net/v/t1.0-9/13226931_1233808183350275_6020287782455920427_n.jpg?_nc_cat=0&oh=78d74adeab14cb24b4af5997c6e7d9d6&oe=5B81C380",
-      description: "Hello...",
-      website: "",
-      social: "",
-      email: "mailto:karatsin@csd.auth.gr"
-    },
+    let randomDevs = [
     {
       name: "Anastasios Theodosiou",
       title: "Full Stack Developer",
